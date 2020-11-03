742c0631b6c516de5bd005edf72ab7276b381eb3
Lecilufa-Kai <lecilufa@gmail.com>
2020-11-04 00:37:17 +0100

    huge change

---
+++ src/test/java/com/hse24/api/service/impl/CategoryServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.hse24.api.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryServiceImplTest {
+
+    @Test
+    void listCategories() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void addCategory() {
+    }
+}
