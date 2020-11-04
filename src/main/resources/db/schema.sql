f8aa048e8928bf49f65b2dcd145d0a796fe4ac0b
Lecilufa-Kai <lecilufa@gmail.com>
2020-11-04 01:11:38 +0100

    huge change

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -9,6 +9,7 @@ create table Product
     id          BIGINT PRIMARY KEY AUTO_INCREMENT,
     name        VARCHAR(40),
     price       DOUBLE,
+    currency    VARCHAR(10),
     category_id bigint,
     foreign key (category_id) references Category (id)
 );
